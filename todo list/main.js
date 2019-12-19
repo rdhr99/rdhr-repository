38b1bacaf547aaf3fbe00c9426d2a16dcf84de52
rdhr99 <59054521+rdhr99@users.noreply.github.com>
2019-12-19 14:10:16 +0100

    Removed and added some useful comments.

--- todo list/main.js
+++ todo list/main.js
@@ -1,17 +1,15 @@
 const button = document.querySelector('#check')
-/*const buttondelete = document.querySelector('.button2')*/
-
-
 const textInput = document.querySelector('.task-type')
 textInput.addEventListener('keydown', (e)=>{
+    /*We listen for the enter (keycode 13)*/
     if(e.keyCode === 13){
         addul(e);
     }
 });
 
-
 const list = document.querySelector('.list-task');
 list.addEventListener('click', (e) =>{
+    /*Find the id of the object we pressed.*/
     const dated = event.target.id;
     if(event.target.type == "button"){
         console.log
@@ -28,75 +26,23 @@ list.addEventListener('click', (e) =>{
 
 
 function finishedTask(e, dateid){
-/*    if(document.querySelector('.task-type').style.textDecoration == 'line-through'){
-        document.querySelector('.task-type').style.textDecoration = 'none';
-        document.querySelector('.task-type').style.color = 'black';
-    }
-    else{
-        document.querySelector('.task-type').style.textDecoration = 'line-through';
-        document.querySelector('.task-type').style.color = '#666';
-    }
-*/
-/*Because of how querySelector works, we need to add \\3*/
-   /*
-    console.log('este es el que tenemos');
-    console.log(dateid);
-    var number = dateid.substring(0,1);
-
-    var res = dateid.substring(1);
-
-    const string = '#\\3'+number+' ' +res;
-    console.log(string);
-    */
-
-    /*aqui está el problema, busca el primero con .tasked, no el de los dos parametros*/
+/*Find the box with the same ID (date) and modify it.*/
     const selectedText = document.querySelector(`.a${dateid}`);
     console.log(selectedText);
     selectedText.style.textDecoration = 'line-through';
     selectedText.style.color = 'rgba(68, 68, 68, 0.692)';
-/*
-    selectedText.style.textDecoration = 'line-through';
-*/
 }
 
 
 
 function unfinishedTask(e,dateid){
-    /*
-    var number = dateid.substring(0,1);
-    var res = dateid.substring(1);
-
-    const string = '#\\3'+number+' ' +res;
-    console.log(string);
-    */
+ 
     const selectedText = document.querySelector(`.a${dateid}`);
     console.log(selectedText);
     selectedText.style.textDecoration = 'none';
     selectedText.style.color = 'black';
 }
 
-/*
-function add(e){
-    const total = document.querySelector('#total');
-    const task = document.createElement('div');
-    task.innerHTML = '<div class="container">' +
-    '<div class="task">' +
-        '<label class="containertask button1">' +
-            '<input type="checkbox"  title="Task completed.">' +
-            '<span class="checkmark" id="check" title="Task completed."></span>' +
-        '</label>' +
-        '<div>' +
-        '<input type="text" class="task-type" name="FirstTask" placeholder="Write here your task...">' +
-        '</div>' +
-        '<div >' +
-            '<input type="button" class="button2  buttonani" title="Delete task" value="✕">' +
-        '</div>' +
-    '</div>' +
-'</div>';
-    total.append(task);
-
-}
-*/
 
 function remove(e, dateid){
     console.log(dateid);
@@ -106,6 +52,7 @@ function remove(e, dateid){
 
 
 function addul(e){
+    /*If we press enter while writing a task it will add it on the UL.*/
     const ul = document.querySelector('.list-task');
     const task = document.createElement('li');
     let taskText = document.querySelector('.task-type').value;
@@ -133,21 +80,3 @@ function addul(e){
 }
 }
 
-
-//Fix the delete button
-//input.addEventListener("keyup", function)
-//if(event.keyCode === 13)
-
-
-/*
-button.addEventListener('click', (e) => {
-    if(document.querySelector('.task-type').style.textDecoration == 'line-through'){
-        document.querySelector('.task-type').style.textDecoration = 'none';
-        document.querySelector('.task-type').style.color = 'black';
-    }
-    else{
-        document.querySelector('.task-type').style.textDecoration = 'line-through';
-        document.querySelector('.task-type').style.color = '#666';
-    }
-})
-*/
